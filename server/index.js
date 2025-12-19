df088abb508cfd71b80ad9e2667ca5d87bdd200e
Nitesh Patil <nitxpatil550@gmail.com>
2025-12-20 02:56:13 +0530

    fix: allow Vercel frontend origin in CORS

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+    "http://localhost:3000",
+    "https://nexora-studio-client.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
