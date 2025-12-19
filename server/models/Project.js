be24ff0c0ddc8d211b95bb7bd52100455ea8f523
Nitesh Patil <nitxpatil550@gmail.com>
2025-12-19 17:49:58 +0530

    Configured MongoDB Atlas connection

---
+++ server/models/Project.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Project", projectSchema);
