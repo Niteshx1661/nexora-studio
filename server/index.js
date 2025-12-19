28fc3b985f1e524a89035bb7de2e532a2f3136b4
Nitesh Patil <nitxpatil550@gmail.com>
2025-12-20 01:06:01 +0530

    Update server configuration and middleware

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
